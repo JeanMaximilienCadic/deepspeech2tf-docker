a1946e6b72f24d33194f93bd1dd52ae0bd4a9e4d
Jean Maximilien Cadic <j.cadic@pm.me>
2021-03-08 14:37:38 +0900

    add sox dependency

--- docker.sh
+++ docker.sh
@@ -1,8 +1,4 @@
+export DATA_DIR="/media/jcadic/cmdrive/ASR/en/librispeech"
 docker rmi -f jcadic/deespeech:tensorflow
 docker build . -t jcadic/deespeech:tensorflow
-
-docker stop jcadic-deepspeech_tensorflow
-docker rm jcadic-deespeech_tensorflow
-
-export DATA_DIR="/mnt/cdata/ASR/en/librispeech"
-docker run -it --name jcadic-deepspeech_tensorflow -v $DATA_DIR:/tmp/librispeech jcadic/deespeech:tensorflow
+docker run -it --rm --name jcadic-deepspeech_tensorflow -v $DATA_DIR:/tmp jcadic/deespeech:tensorflow
